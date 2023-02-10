4de1d56ab835c85a221736ef1cd5d84dbfc6b2a5
Mohammad Shanoor <107303734+shanmd975@users.noreply.github.com>
2023-02-10 14:33:01 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,25 @@ import '../styles/App.css';
 
 const App = () => {
 //code here 
+  const  [color,setColor] = useState('red');
+  const [fontStyle, setFontStyle] = useState('normal');
+  
+  const handleClick = () => {
+    if(color =='red') {
+      setColor('blue');
+      setFontStyle('italic');
+    }else{
+      setColor('red');
+      setFontStyle('normal');
+    }
+  };
   return (
     <div id="main">
       <p className={} >Newton School</p>
       <button id='button' onClick={}>Change Style</button>
     </div>
-  )
-}
+  );
+};
 
 
 export default App;
